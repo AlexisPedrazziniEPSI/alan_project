ba49ba7129904d138a1db0a949de8387800b1328
AlexisPedrazziniEPSI <145668867+AlexisPedrazziniEPSI@users.noreply.github.com>
2024-10-17 12:36:14 +0200

    Update de fin de cours

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,12 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Request  $request): Response
     {
 
         /* // aller chercher les noms des dossiers dans public/photos
@@ -23,13 +27,31 @@ class HomeController extends AbstractController
         $finder = new Finder();
         $dossier = $finder->directories()->in('photos');
 
+        // on va ajouter un formulaire
+        $form = $this->CreateFormBuilder()
+            ->add('dossier', TextType::class, ['label' => 'Nom du dossier'])
+            ->add("ajouter", SubmitType::class, ['label' => 'Ajouter un dossier'])
+            ->getForm();
+
+        // gestion POST
+        $form->handleRequest($request); // on regarde si le formulaire a été soumis
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $dossier = $data['dossier'];
+            $fs = new Filesystem();
+            $fs->mkdir("photos/$dossier");
+
+            return $this->redirectToRoute('app_chatons', ['dossier' => $dossier]); // on redirige vers la page chatons
+        }
+
         return $this->render('home/index.html.twig', [
-            'dossiers' => $dossier
+            'dossiers' => $dossier,
+            'formulaire' => $form->createView()
         ]);
     }
 
     #[Route("/chatons/{dossier}", name: 'app_chatons')]
-    public function chatons($dossier): Response
+    public function chatons($dossier, Request $request): Response
     {
         $fs = new Filesystem();
         $chemin = "photos/$dossier";
@@ -37,12 +59,25 @@ class HomeController extends AbstractController
             throw $this->createNotFoundException("Le dossier $dossier n'existe pas");
         }
 
+        $form = $this->CreateFormBuilder()
+            ->add('photo', FileType::class, ['label' => 'Nom de la photo'])
+            ->add("ajouter", SubmitType::class, ['label' => 'Ajouter une photo'])
+            ->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            $photo = $data['photo'];
+            $photo->move($chemin, $photo->getClientOriginalName());
+            return $this->redirectToRoute('app_chatons', ['dossier' => $dossier]);
+        }
+
         $finder = new Finder();
         $photos = $finder->files()->in($chemin);
 
         return $this->render('home/liste_chatons.html.twig', [
             'nom_dossier' => $dossier,
-            'photos' => $photos
+            'photos' => $photos,
+            'formulaire' => $form->createView()
         ]);
     }
 }
